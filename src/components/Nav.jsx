899d380a99ce8bb961757777354daaa43cae750a
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-05-31 00:46:45 +0200

    Nav.jsx utilise useNavigate au lieu des liens

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import logo from '../assets/logo.svg';
 import classes from '../styles/Nav.module.css';
 
 function Nav() {
 
   const location = useLocation().pathname;
+  const navigate = useNavigate();
 
   return (
     <header className={classes.header}>
-      <a href='../'>
-        <img src={logo} className={classes.kasa_logo} alt="logo du site Kasa" />
-      </a>
+        <img src={logo} className={classes.kasa_logo} alt="logo du site Kasa" onClick={() => navigate("../")}/>
       <nav className={classes.header_nav}>
-        <a href='../' style={{
+        <p onClick={() => navigate("../")} style={{
           textDecoration: location === '/' ? `underline` : 'auto'
-        }}>Accueil</a>
-        <a href='../about' style={{
+        }}>Accueil</p>
+        <p onClick={() => navigate("../about")} style={{
           textDecoration: location === '/about' ? `underline` : 'auto'
-        }}>A Propos</a>
+        }} >A Propos</p>
       </nav>
     </header>
   );
