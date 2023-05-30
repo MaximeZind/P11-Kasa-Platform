42a92e9d77e3f94393f1d478478b9917620b392a
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-05-30 23:29:33 +0200

    composant Nav modifié, il prend en compte l'url

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import logo from '../assets/logo.svg';
 import classes from '../styles/Nav.module.css';
 
 function Nav() {
+
+  const location = useLocation().pathname;
+  console.log(location);
+
   return (
-    <header className={classes.banner}>
+    <header className={classes.header}>
       <a href='../'>
-        <img src={logo} className="kasa-logo" alt="logo du site Kasa" />
+        <img src={logo} className={classes.kasa_logo} alt="logo du site Kasa" />
       </a>
-      <nav className={classes.banner_nav}>
-        <a href='../'>Accueil</a>
-        <a href='../about'>A Propos</a>
+      <nav className={classes.header_nav}>
+        <a href='../' style={{
+          textDecoration: location === '/' ? `underline` : 'auto'
+        }}>Accueil</a>
+        <a href='../about' style={{
+          textDecoration: location === '/about' ? `underline` : 'auto'
+        }}>A Propos</a>
       </nav>
     </header>
-
   );
 }
 
