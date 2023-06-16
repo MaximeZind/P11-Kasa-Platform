aa6240c410a60c177e3a592ef3e8297535874651
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-06-16 16:48:11 +0200

    modifications du collapse, et mise à jour du composant nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,24 @@
 import React from 'react';
-import { useNavigate, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.svg';
 import classes from '../styles/Nav.module.css';
 
 function Nav() {
-  
-  const navigate = useNavigate();
 
   return (
     <header className={classes.header}>
-      <img src={logo} className={classes.kasa_logo} alt="logo du site Kasa" onClick={() => navigate("../")} />
+      <NavLink to="../">
+      <img src={logo} className={classes.kasa_logo} alt="logo du site Kasa" />
+      </NavLink>
       <nav className={classes.header_nav}>
-        <NavLink
-          to="../"
-          style={({ isActive }) => {
+        <NavLink to="../" style={({ isActive }) => {
             return {
               textDecoration: isActive ? `underline` : 'auto',
             };
-          }}
-        >
+          }}>
           Accueil
         </NavLink>
-        <NavLink
-          to="../about"
-          style={({ isActive }) => {
+        <NavLink to="../about" style={({ isActive }) => {
             return {
               textDecoration: isActive ? `underline` : 'auto',
             };
