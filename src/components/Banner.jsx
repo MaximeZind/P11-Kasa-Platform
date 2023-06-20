a245cdf5675ffa0f83c9a44ab91e9806f486c132
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-06-20 11:35:18 +0200

    rectification bug page title

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,9 +4,6 @@ import classes from '../styles/Banner.module.css';
 
 function Banner({title, heroImg, altText}) {
 
-    const pageTitle = 'A Propos';
-    document.title = `Kasa - ${pageTitle}`;
-
     return (
             <div className={classes.banner}>
                 <img src={heroImg} alt={altText}></img>
