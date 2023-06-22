7f2f2ce2396ba4dd4cf1626d1e86595777a1154e
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-06-22 19:19:46 +0200

    code react router dans un dossier à part

--- src/utils/accommodationUtils.js
+++ src/utils/accommodationUtils.js
@@ -2,6 +2,7 @@ import { getLogements } from "./import";
 
 const accommodations = getLogements();
 
+//prend en paramètre un ID, et renvoie le logement correspondant
 export function getLogement(id){
     const selectedAccommodation = accommodations.find((accommodation) => accommodation.id === id);
     return selectedAccommodation;
